29362b3134760f5bb467bb3147fa58e216f43fc1
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-01-27 00:15:37 +0800

    Remove unused defines

--- src/main.hpp
+++ src/main.hpp
@@ -1,13 +1,6 @@
 #ifndef __MAIN_HPP__
 #define __MAIN_HPP__
 
-/* Bluetooth buffer related */
-#define TX_BUF_SIZE 200
-
-/* Control action related */
-#define CMD_START   'j'
-#define CMD_STOP    'k'
-
 #define UPDATE_INT 50 // 50ms, 20FPS
 
 /* Type definitions */
