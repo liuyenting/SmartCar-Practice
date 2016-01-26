c4c4e0ed08b8a286d01b1804cc42fecb027f9685
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-01-26 23:49:27 +0800

    Modify for CCD demo

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,8 @@
 #include <libbase/k60/mcg.h>
 #include <libsc/system.h>
 #include <libbase/k60/gpio.h>
-#include <libsc/k60/jy_mcu_bt_106.h>
-#include <libsc/futaba_s3010.h>
-#include <libsc/alternate_motor.h>
+#include <libsc/st7735r.h>  // LCD
+#include <libsc/tsl1401cl.h> // CCD
 
 #include "main.hpp"
 
@@ -26,7 +25,7 @@ Mcg::Config Mcg::GetMcgConfig() {
 using namespace libsc;
 using namespace libbase::k60;
 
-
+std::array<uint16_t, Tsl1401cl::kSensorW> ccd_data;
 
 int main(void) {
 	System::Init();
@@ -36,43 +35,20 @@ int main(void) {
 	init(peripherals);
 
 	while(true) {
-		/* Put the state machine here.... */
+
+		System::DelayMs(UPDATE_INT);
 	}
 
 	return 0;
 }
 
 void init(struct peripherals_t &peripherals) {
-	// Init bluetooth.
-	libsc::k60::JyMcuBt106::Config bluetooth_config;
-	bluetooth_config.id = 0;       // UART0
-	bluetooth_config.baud_rate = Uart::Config::BaudRate::k115200;
-	bluetooth_config.tx_buf_size = TX_BUF_SIZE;
-	bluetooth_config.rx_isr = bluetooth_listener;
-	peripherals.bluetooth = new libsc::k60::JyMcuBt106(bluetooth_config);
-	//Init motor.
-	AlternateMotor::Config motorConfig;
-	motorConfig.id = 0;
-	peripherals.driving = new AlternateMotor(motorConfig);
-}
+	// Init the LCD hardware.
+	St7735r::Config st7735r_config;
+	st7735r_config.is_revert = false;
+	st7735r_config.is_bgr = false;
+	peripherals->lcd = new St7735r(st7735r_config);
 
-bool bluetooth_listener(const Byte *data, const size_t data_size) {
-	// Take the first byte in the array as our current action to process.
-	Byte action = data[0];
-	switch(action) {
-	case CMD_START:
-		/* Start the car */
-
-		break;
-	case CMD_STOP:
-		/* Stop the car */
-
-		break;
-	default:
-		// Unknown command.
-		break;
-	}
-return true;
-// Discard rest of the data.
-
+	// Init the linear CCD.
+	peripherals->ccd = new Tsl1401cl(0);
 }
